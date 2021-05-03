d7c918bd4949b4e5e4f3b45468f17d4910613086
B103 <71344130+B102@users.noreply.github.com>
2021-05-03 09:01:29 -0500

    Update JMap.java

--- JMap.java
+++ JMap.java
@@ -1,9 +1,57 @@
 package numbers;
 
+import java.util.Arrays;
+
 class JMap {
-    private JMapCell[][] mapInfo;
-    void load(JMapCell[][] map){
-        mapInfo = map;
-        System.out.println("Map loaded");
+    
+    layer[] layers;
+    sample[] sampleSet;
+    double[][] target;
+    
+    void update(JMapCell[][] image, int h, int w){
+        target = new double[w][h];
+        for (int y = 0; y < h; y++){
+            for (int x = 0; x < w; x++){
+                target[x][y] = image[x][y].colorVal();
+            }
+        }
+    }
+    
+    void print2D(){
+        for (double[] row : target){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+    
+    int predict(){
+        //does one forward pass
+        //return 0 for now
+        return 0;
+    }
+    
+    void iterate(){
+        //set weight
+        //create layers
+        //import training data
+        //train
+    }
+    
+    void forward(double[][] inputs){
+        //bring the inputs into the input layer layers[0]
+        //add in the bias 
+    }
+    
+    void backward(double r, sample s) {
+        //calculate a gradient and store the updated weights in the neurons
+        //(calculate the output layer weights, calculate the hidden layer weight then update all the weights)
+    }
+    
+    void train(int iterations,float rate) {
+    	for(int i = 0; i < iterations; i++) {
+            for(int j = 0; j < sampleSet.length; j++) {
+                forward(sampleSet[j].data);
+                backward(rate,sampleSet[j]);
+            }
+    	}
     }
 }
