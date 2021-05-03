165a8b30552c034412fce8e4042be484c23e93e0
B103 <71344130+B102@users.noreply.github.com>
2021-05-02 20:19:47 -0500

    Create JMapCell.java

---
+++ JMapCell.java
@@ -0,0 +1,45 @@
+package numbers;
+
+import java.awt.*;
+import javax.swing.*;
+
+class JMapCell extends JComponent{
+    private static final Dimension CELL_SIZE = new Dimension(9, 9);
+    private int Drawn = 0;
+    int xCoord;
+    int yCoord;
+    
+    boolean path = false;
+    JMapCell(int color){
+        setPreferredSize(CELL_SIZE);
+        setColor(color);
+    }
+    
+    JMapCell(){
+        setPreferredSize(CELL_SIZE);
+        setColor(0);
+    }
+    
+    void setColor(int color){
+        Drawn = color;
+        updateAppearance();
+    }
+    
+    int colorVal(){
+        return Drawn;
+    }
+    
+    private void updateAppearance(){
+        if (Drawn != 0){
+            setBackground(new java.awt.Color(Drawn, Drawn, Drawn));
+        }else{
+            setBackground(Color.WHITE);
+        }
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g){
+        g.setColor(getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+}
