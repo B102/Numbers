e2ca89945fe8e83a271881367433ca86433c38d7
B103 <71344130+B102@users.noreply.github.com>
2021-05-03 09:12:12 -0500

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,13 @@
+package numbers;
+
+public class sample {
+    
+    double[][] data;
+    int expectedOutput;
+   
+    public sample(double[][] data, int expectedOutput) {
+        //translate
+        this.data = data;
+        this.expectedOutput = expectedOutput;
+    } 
+}
