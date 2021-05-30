983eeaa896b93a80c0ae51979aaa89c67b8d320c
B103 <71344130+B102@users.noreply.github.com>
2021-05-30 13:21:19 -0500

    Update layer.java

--- layer.java
+++ layer.java
@@ -1,5 +1,20 @@
 package numbers;
 
 public class layer {
-    
+    double[] components;
+    double bias;
+    final int INPUT_DATA_SIZE = 1024;
+    final int OUTPUT_DATA_SIZE = 10;
+    final int LAYER_SIZE = 16;
+    layer(String id){
+        if (id == "inputL"){
+            components = new double[INPUT_DATA_SIZE];
+        }
+        if (id == "outputL"){
+            components = new double[OUTPUT_DATA_SIZE];
+        }
+        if (id == "hiddenL"){
+            components = new double[LAYER_SIZE];
+        }
+    }
 }
